a62c848708906a13789875171a0dabd6a5d6fa82
Sakshi-97 <40206898+Sakshi-97@users.noreply.github.com>
2018-08-16 13:59:41 +0530

    Add files via upload

---
+++ start.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+
+from NoteDB import NoteDB
+from dashboard import Dashboard
+if __name__=="__main__":
+    try:
+        #Add Your Database username and password here
+        db=NoteDB(username='manik',password='manik')
+        Dashboard().initUI(db)
+    except Exception as e:
+     messagebox.showinfo("Error","Unable to establish database connection.")
